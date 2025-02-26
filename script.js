188f3561a3c3992da555be236f308dabfb2f33a2
TSjhkim <jhkim0228@tangunsoft.com>
2025-02-26 23:29:12 +0900

    swith문 공부

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+const name = 'minhee';
+switch(name) {
+  case 'minhee' :
+    console.log('my girl');
+    break;
+  default :
+    console.log('not my girl');
+    break;
+}
