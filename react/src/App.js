b244a114142ffea5e3001997d5aedcc3730ea730
TSjhkim <jhkim0228@tangunsoft.com>
2025-03-17 20:38:50 +0900

    finish reach

--- react/src/App.js
+++ react/src/App.js
@@ -1,45 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Todolist from './components/Todolist.js'
 import './style.css';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-
-  const addNewTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodoList([...todoList, newTodo]);
-      setNewTodo('');
-    }
-  };
-
-  const deleteTodo = (index) => {
-    const updateList = [
-      ...todoList.slice(0, index),
-      ...todoList.slice(index + 1),
-    ];
-    setTodoList(updateList);
-  };
-
-  return (
-    <div>
-      <h1>To Do List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => {
-          setNewTodo(e.target.value);
-        }}
-      />
-      <button onClick={addNewTodo}>추가</button>
-      <ul>
-        {todoList.map((todo, index) => (
-          <li key={index}>
-            {todo} <button onClick={() => deleteTodo(index)}>삭제</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <Todolist style={{color:'blue'}} />;
 }
 
 export default App;
---
+++ react/src/components/Todolist.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+function Todolist(props){
+    const [todoList, setTodoList] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+  
+    const addNewTodo = () => {
+      if (newTodo.trim() !== '') {
+        setTodoList([...todoList, newTodo]);
+        setNewTodo('');
+      }
+    };
+  
+    const deleteTodo = (index) => {
+      const updateList = [
+        ...todoList.slice(0, index),
+        ...todoList.slice(index + 1),
+      ];
+      setTodoList(updateList);
+    };
+
+    return (
+      <div>
+        <h1 style={props.style}>To Do List</h1>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+        <button onClick={addNewTodo}>추가</button>
+        <ul>
+          {todoList.map((todo, index) => (
+            <li key={index}>
+              {todo} <button onClick={() => deleteTodo(index)}>삭제</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+}
+
+export default Todolist;
